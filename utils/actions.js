b133880a95ea1441bf31082c760925c34caf313a
bobbidigi <bobbidigi34@gmail.com>
2020-03-20 14:12:25 -0400

    fix karaoke id

--- utils/actions.js
+++ utils/actions.js
@@ -16,7 +16,7 @@ export function fetchVenuesByLatLng(location){
 
     const ID = process.env.ID;
     const SECRET = process.env.SECRET;
-
-    const karaoke = "5744ccdfe4b0c0459246b4bb" 
+    const karaoke = "4bf58dd8d48988d120941735";
+    const karaokeBox = "5744ccdfe4b0c0459246b4bb"; 
     return axios.get(`https://api.foursquare.com/v2/venues/search?client_id=${ID}&client_secret=${SECRET}&v=20180323&limit=2&near=${location}&categoryId=${karaoke}&&radius=10000`)
 }
