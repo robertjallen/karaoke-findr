99743051ed72697609e8f8bf22909f2833c907bf
bobbidigi <bobbidigi34@gmail.com>
2020-03-20 14:53:45 -0400

    test

--- utils/actions.js
+++ utils/actions.js
@@ -14,8 +14,9 @@ export function fetchVenuesByLatLng(location){
         id: process.env.id,
       })
 
-    const ID = process.env.ID;
-    const SECRET = process.env.SECRET;
+    const ID = process.env.id;
+    const SECRET = process.env.secret;
+    
     const karaoke = "4bf58dd8d48988d120941735";
     const karaokeBox = "5744ccdfe4b0c0459246b4bb"; 
     return axios.get(`https://api.foursquare.com/v2/venues/search?client_id=${ID}&client_secret=${SECRET}&v=20180323&limit=2&near=${location}&categoryId=${karaoke}&&radius=10000`)
