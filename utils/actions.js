a9c56dfcb6b6c48bbd2f2c501660ec7625eaf9a3
bobbidigi <bobbidigi34@gmail.com>
2020-03-20 15:54:59 -0400

    add increase to limit

--- utils/actions.js
+++ utils/actions.js
@@ -16,8 +16,8 @@ export function fetchVenuesByLatLng(location){
 
     const ID = process.env.id;
     const SECRET = process.env.secret;
-    
+
     const karaoke = "4bf58dd8d48988d120941735";
     const karaokeBox = "5744ccdfe4b0c0459246b4bb"; 
-    return axios.get(`https://api.foursquare.com/v2/venues/search?client_id=${ID}&client_secret=${SECRET}&v=20180323&limit=2&near=${location}&categoryId=${karaoke}&&radius=10000`)
+    return axios.get(`https://api.foursquare.com/v2/venues/search?client_id=${ID}&client_secret=${SECRET}&v=20180323&limit=20&near=${location}&categoryId=${karaoke}&&radius=10000`)
 }
